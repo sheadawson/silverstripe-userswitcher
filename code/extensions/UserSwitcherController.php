6695476af776517bc9ce69cec65035556f148a65
spekulatius <petert@catalyst.net.nz>
2015-12-13 01:51:10 +1300

    Converted to PSR-2

--- code/extensions/UserSwitcherController.php
+++ code/extensions/UserSwitcherController.php
@@ -5,58 +5,61 @@
  * @author Shea Dawson <shea@livesource.co.nz>
  * @license BSD http://silverstripe.org/bsd-license/
  */
-class UserSwitcherController extends Controller {
+class UserSwitcherController extends Controller
+{
 
-	Const URLSegment = 'userswitcher';
+    const URLSegment = 'userswitcher';
 
-	public static $allowed_actions = array(
-		'UserSwitcherForm',
-	);
+    public static $allowed_actions = array(
+        'UserSwitcherForm',
+    );
 
-	public function UserSwitcherForm() {
-		if(Director::isLive()){
-			return false;
-		}
+    public function UserSwitcherForm()
+    {
+        if (Director::isLive()) {
+            return false;
+        }
 
-		if(Permission::check('ADMIN') || Session::get('UserSwitched')) {
-			$members = Member::get()->map()->toArray();
-				
-			if(isset($_GET['userswitchercms']) && $_GET['userswitchercms'] == 1) {
-				$field = DropdownField::create('MemberID', '', $members)
-					->setEmptyString(_t('UserSwticherController.SwitchUser','Switch User'));
-			} else {
-				$field = DropdownField::create('MemberID', 'User:', $members, Member::currentUserID());
-			}
+        if (Permission::check('ADMIN') || Session::get('UserSwitched')) {
+            $members = Member::get()->map()->toArray();
+                
+            if (isset($_GET['userswitchercms']) && $_GET['userswitchercms'] == 1) {
+                $field = DropdownField::create('MemberID', '', $members)
+                    ->setEmptyString(_t('UserSwticherController.SwitchUser', 'Switch User'));
+            } else {
+                $field = DropdownField::create('MemberID', 'User:', $members, Member::currentUserID());
+            }
 
-			$fields = FieldList::create($field);
+            $fields = FieldList::create($field);
 
-			$actions = FieldList::create(
-				FormAction::create('switchuser', 'Switch User')
-			);
+            $actions = FieldList::create(
+                FormAction::create('switchuser', 'Switch User')
+            );
 
-			$validator = RequiredFields::create(
-				'MemberID'
-			);
+            $validator = RequiredFields::create(
+                'MemberID'
+            );
 
-			return Form::create($this, 'UserSwitcherForm', $fields, $actions, $validator)
-				->addExtraClass('userswitcher');
-		}
-	}
+            return Form::create($this, 'UserSwitcherForm', $fields, $actions, $validator)
+                ->addExtraClass('userswitcher');
+        }
+    }
 
-	public function switchuser($data, $form) {
-		if(Permission::check('ADMIN') || Session::get('UserSwitched')){	
-			if($member = Member::get()->byID((int)$data['MemberID'])) {
-				$member->logIn();
-				Session::set('UserSwitched', 1);
-				return $this->redirectBack();
-			}
-		} else {
-			return $this->httpError('404');
-		}
-	}
+    public function switchuser($data, $form)
+    {
+        if (Permission::check('ADMIN') || Session::get('UserSwitched')) {
+            if ($member = Member::get()->byID((int)$data['MemberID'])) {
+                $member->logIn();
+                Session::set('UserSwitched', 1);
+                return $this->redirectBack();
+            }
+        } else {
+            return $this->httpError('404');
+        }
+    }
 
-	public function Link($action = '') {
-		return self::URLSegment . '/' . $action;
-	}
-
+    public function Link($action = '')
+    {
+        return self::URLSegment . '/' . $action;
+    }
 }
