7690b350b3569f0b73d41c26994b90f06d18c7b5
deracs <michael@deracs.com>
2016-04-22 16:32:45 +1200

    Update UserSwitcherControllerExtension.php

--- code/extensions/UserSwitcherControllerExtension.php
+++ code/extensions/UserSwitcherControllerExtension.php
@@ -14,11 +14,21 @@ class UserSwitcherControllerExtension extends Extension
 
     public function onAfterInit()
     {
+        $isCMS = substr($this->owner->getRequest()->getURL(), 0, 5) == 'admin' || (int)$this->owner->getRequest()->getVar('userswitchercms') == 1;
+
         if ($this->providUserSwitcher()) {
             Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
             Requirements::javascript(THIRDPARTY_DIR . '/jquery-entwine/dist/jquery.entwine-dist.js');
             Requirements::javascript(FRAMEWORK_DIR  . '/javascript/jquery-ondemand/jquery.ondemand.js');
-            Requirements::javascript(USERSWITCHER    . '/javascript/userswitcher.js');
+
+            if ($isCMS) {
+                    Requirements::javascript(USERSWITCHER . '/javascript/userswitcher.js');
+            } else {
+                if(SiteConfig::current_site_config()->Toggle == 1) {
+                    Requirements::javascript(USERSWITCHER . '/javascript/userswitcher.js');
+                }
+            }
+
         }
     }
 
