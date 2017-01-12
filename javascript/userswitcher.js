7ff9bd2950a3a6c6794de6ac0e6803106bca1d71
Jake Bentvelzen <jake@silverstripe.com.au>
2017-01-12 12:00:31 +1100

    fix(UserSwitcher): Stop most of the logic from using entwine

--- javascript/userswitcher.js
+++ javascript/userswitcher.js
@@ -1,40 +1,47 @@
 (function($){
-	$.entwine('userswitcher', function($){
+	function loadSwitcher() {
+		var base = $('base').prop('href');
+		var isCMS = $('body').hasClass('cms') ? 1 : 0;
 
-		$('form.userswitcher select').entwine({
-			onchange : function(){
-				this.parents('form:first').submit();
-				this._super();
+		$.get(base + 'userswitcher/UserSwitcherFormHTML/', {userswitchercms: isCMS}).done(function(data){
+			var $data = $(data);
+			if (!$data.length) {
+				return;
+			}
+			// Submit form on change
+			$data.find('select').on('change', function() {
+				$(this.form).submit();
+			});
+			// Hide submit button
+			$data.find('.Actions').hide();
+
+			if($('body').hasClass('cms')){
+				$('.cms-login-status').append($data);
+			}else{
+				$('body').append($data);	
 			}
 		});
+	}
 
-		$('form.userswitcher .Actions').entwine({
-			onmatch : function(){
-				this.hide();
-				this._super();
-			}
-		});
-
-		$('body').entwine({
-			onmatch : function(){
-				var base = $('base').prop('href'),
-					isCMS = this.hasClass('cms') ? 1 : '';
-				
-				$.get(base + 'userswitcher/UserSwitcherFormHTML/', {userswitchercms: isCMS}).done(function(data){
-					var body = $('body');
-
-					if(body.hasClass('cms')){
-						$('.cms-login-status').append(data);
-					}else{
-						$('body').append(data);	
+	function main() {
+		var isCMS = $('body').hasClass('cms') ? 1 : 0;
+		if (!$.entwine || !isCMS) {
+			loadSwitcher();
+		} else {
+			$.entwine('userswitcher', function($){
+				$('body').entwine({
+					onmatch : function(){
+						loadSwitcher();
+						this._super();
+					},
+					onunmatch: function() {
+						this._super();
 					}
-				  	
 				});
-				this._super();
-			}
-		});
-		
-	});
+			});
+		}
+	}
+	main();
 })(jQuery);
 
  
